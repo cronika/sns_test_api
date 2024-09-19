e109fa699bdea01489fb94d0ad7220934584f204
cronika <chronicale5000@gmail.com>
2024-09-19 17:56:07 +0900

    PORT

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 require("dotenv").config;
 
-const PORT = 5000;
+const PORT = process.env.PORT || 10000;
 
 // app.get("/", (req, res) => {
 //   res.send("<h1>Hello</h1>");
